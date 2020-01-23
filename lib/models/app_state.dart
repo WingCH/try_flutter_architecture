792393b7dd78b6c0192f4e74dfc62db2dd3f5249
WingCH <wingchan@vgt-hk.net>
2020-01-23 13:10:39 +0800

    copy model file from original repositories

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -2,102 +2,58 @@
 // Use of this source code is governed by the MIT license that can be found
 // in the LICENSE file.
 
+import 'package:meta/meta.dart';
+import 'package:try_flutter_architecture/models/models.dart';
 
-import '../helper/uuid.dart';
+
+@immutable
 class AppState {
-  bool isLoading;
-  List<Todo> todos;
+  final bool isLoading;
+  final List<Todo> todos;
+  final AppTab activeTab;
+  final VisibilityFilter activeFilter;
 
-  AppState({
-    this.isLoading = false,
-    this.todos = const [],
-  });
+  AppState(
+      {this.isLoading = false,
+        this.todos = const [],
+        this.activeTab = AppTab.todos,
+        this.activeFilter = VisibilityFilter.all});
 
-//  factory AppState.loading() => AppState(isLoading: true);
+  factory AppState.loading() => AppState(isLoading: true);
 
-  bool get allComplete => todos.every((todo) => todo.complete);
-
-  List<Todo> filteredTodos(VisibilityFilter activeFilter) =>
-      todos.where((todo) {
-        switch (activeFilter) {
-          case VisibilityFilter.active:
-            return !todo.complete;
-          case VisibilityFilter.completed:
-            return todo.complete;
-          case VisibilityFilter.all:
-          default:
-            return true;
-        }
-      }).toList();
-
-  bool get hasCompletedTodos => todos.any((todo) => todo.complete);
-
-  @override
-  int get hashCode => todos.hashCode ^ isLoading.hashCode;
-
-  int get numActive =>
-      todos.fold(0, (sum, todo) => !todo.complete ? ++sum : sum);
-
-  int get numCompleted =>
-      todos.fold(0, (sum, todo) => todo.complete ? ++sum : sum);
-
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is AppState &&
-          runtimeType == other.runtimeType &&
-          todos == other.todos &&
-          isLoading == other.isLoading;
-
-  void clearCompleted() {
-    todos.removeWhere((todo) => todo.complete);
+  AppState copyWith({
+    bool isLoading,
+    List<Todo> todos,
+    AppTab activeTab,
+    VisibilityFilter activeFilter,
+  }) {
+    return AppState(
+      isLoading: isLoading ?? this.isLoading,
+      todos: todos ?? this.todos,
+      activeTab: activeTab ?? this.activeTab,
+      activeFilter: activeFilter ?? this.activeFilter,
+    );
   }
-
-  void toggleAll() {
-    final allCompleted = allComplete;
-
-    todos.forEach((todo) => todo.complete = !allCompleted);
-  }
-
-  @override
-  String toString() {
-    return 'AppState{todos: $todos, isLoading: $isLoading}';
-  }
-}
-
-enum AppTab { todos, stats }
-
-enum ExtraAction { toggleAllComplete, clearCompleted }
-
-class Todo {
-  bool complete;
-  String id;
-  String note;
-  String task;
-
-  Todo(this.task, {this.complete = false, this.note = '', String id})
-      : id = id ?? Uuid().generateV4();
 
   @override
   int get hashCode =>
-      complete.hashCode ^ task.hashCode ^ note.hashCode ^ id.hashCode;
+      isLoading.hashCode ^
+      todos.hashCode ^
+      activeTab.hashCode ^
+      activeFilter.hashCode;
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is Todo &&
-          runtimeType == other.runtimeType &&
-          complete == other.complete &&
-          task == other.task &&
-          note == other.note &&
-          id == other.id;
+          other is AppState &&
+              runtimeType == other.runtimeType &&
+              isLoading == other.isLoading &&
+              todos == other.todos &&
+              activeTab == other.activeTab &&
+              activeFilter == other.activeFilter;
 
   @override
   String toString() {
-    return 'Todo{complete: $complete, task: $task, note: $note, id: $id}';
+    return 'AppState{isLoading: $isLoading, todos: $todos, activeTab: $activeTab, activeFilter: $activeFilter}';
   }
-
 }
-
-enum VisibilityFilter { all, active, completed }
---
+++ lib/models/todo.dart
@@ -0,0 +1,47 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+
+import 'package:meta/meta.dart';
+import 'package:try_flutter_architecture/helper/uuid.dart';
+
+@immutable
+class Todo {
+  final bool complete;
+  final String id;
+  final String note;
+  final String task;
+
+  Todo(this.task, {this.complete = false, String note = '', String id})
+      : note = note ?? '',
+        id = id ?? Uuid().generateV4();
+
+  Todo copyWith({bool complete, String id, String note, String task}) {
+    return Todo(
+      task ?? this.task,
+      complete: complete ?? this.complete,
+      id: id ?? this.id,
+      note: note ?? this.note,
+    );
+  }
+
+  @override
+  int get hashCode =>
+      complete.hashCode ^ task.hashCode ^ note.hashCode ^ id.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Todo &&
+              runtimeType == other.runtimeType &&
+              complete == other.complete &&
+              task == other.task &&
+              note == other.note &&
+              id == other.id;
+
+  @override
+  String toString() {
+    return 'Todo{complete: $complete, task: $task, note: $note, id: $id}';
+  }
+}
